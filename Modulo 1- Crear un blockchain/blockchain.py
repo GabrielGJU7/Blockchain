12a6fb37d38a03df8b247a7daefe761c8d88f74c
GabrielGJU7 <gjacoboulloa@gmail.com>
2021-11-12 20:03:59 -0600

    Creando metodo para obtener hash de un bloque

--- Modulo 1- Crear un blockchain/blockchain.py
+++ Modulo 1- Crear un blockchain/blockchain.py
@@ -6,6 +6,7 @@ import datetime
 import hashlib
 import json
 from flask import Flask, jsonify
+from flask.json import JSONDecoder, dump
 
 # Parte 1 - Crear la cadena de Bloques
 
@@ -31,11 +32,15 @@ class Blockchain:
     def proof_of_word(self, previous_proof):
         new_proof = 1
         check_proof = False
-        
+
         while check_proof is False:
             hash_operation = hashlib.sha256( str(new_proof ** 2 - previous_proof ** 2).encode()).hexdigest()
             if hashlib[:4] == '0000':
                 check_proof = True
             else: new_proof  += 1    
-        return new_proof        
+        return new_proof
+
+    def hash(self, block):
+        encoded_block = json.dumps(block, sort_keys = True).encode()
+        return hashlib.sha256(encoded_block).hexdigest()            
 # Parte 2 - Minado de un bloque de la cadena
